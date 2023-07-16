fc205e7af18b8e893e8ce481d9c8eee8c1c47a3e
Kelii DeLeon <keliikeideleon@gmail.com>
2023-07-16 15:38:06 -0600

    Working storage on todo app

--- JS Todo Exercise/app.js
+++ JS Todo Exercise/app.js
@@ -2,44 +2,66 @@ const formElement = document.querySelector("form");
 const newTaskInput = document.querySelector("#taskName");
 const taskList = document.querySelector("#taskContainer");
 let numberOfTasks = 0;
-/*console.log(JSON.parse(localStorage.getItem("todos")));
 let savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
 
-if (savedTodos) {
+if (savedTodos != []) {
     for (todo of savedTodos) {
+        console.log(todo);
         createTask(todo.name, todo.checked);
-        numberOfTasks++;
     }
-}*/
+}
 
 formElement.addEventListener("submit", function(e){
     e.preventDefault();
 
     // Create and append new Task
     if (newTaskInput.value) {
-        createTask(newTaskInput.value, false);
+        createNewTask(newTaskInput.value);
         newTaskInput.value = '';
     }
 })
 
-taskList.addEventListener("click", function(event) {
-    if (event.target.tagName === "BUTTON") {
-        event.target.parentElement.remove();
+taskList.addEventListener("click", function(e) {
+    if (e.target.tagName === "BUTTON") {
+        const desiredId = e.target.previousSibling.previousSibling.id;
+        const foundIndex = savedTodos.findIndex(obj => obj.taskId === desiredId);
+
+        if (foundIndex !== -1) {
+            const removedObject = savedTodos.splice(foundIndex, 1);
+            console.log('Removed object:', removedObject[0]);
+        }
+        localStorage.setItem("todos", JSON.stringify(savedTodos));
+        e.target.parentElement.remove();
     }
-    });
+});
 
 function taskComplete(checkboxElement) {
+    const desiredId = checkboxElement.id;
+    const foundIndex = savedTodos.findIndex(obj => obj.taskId === desiredId);
+    const foundObject = savedTodos[foundIndex];
     if (checkboxElement.checked) {
         checkboxElement.nextElementSibling.classList.add("checked");
+        if (foundObject) {
+            foundObject.checked = true;
+        }
     } else {
         checkboxElement.nextElementSibling.classList.remove("checked");
+        if (foundObject) {
+            foundObject.checked = false;
+        }
     }
+    console.log(savedTodos);
+    savedTodos.splice(foundIndex, 1, foundObject);
+    console.log(savedTodos);
+    localStorage.setItem("todos", JSON.stringify(savedTodos));
 }
 
 function createTask(taskName, isChecked) {
+    // Create wrapper div
     const taskDiv = document.createElement("div");
     taskDiv.classList.add("task");
 
+    // Create checkbox
     const taskCheckbox = document.createElement("input");
     taskCheckbox.setAttribute("type", "checkbox");
     taskCheckbox.setAttribute("id", `task${numberOfTasks}`);
@@ -47,18 +69,30 @@ function createTask(taskName, isChecked) {
     taskCheckbox.setAttribute("onchange", "taskComplete(this)");
     if(isChecked) {taskCheckbox.setAttribute("checked", '');}
 
+    // Create task label
     const taskLabel = document.createElement("label");
     taskLabel.setAttribute('for', `task${numberOfTasks}`);
     taskLabel.innerText = taskName;
 
+    // Create remove task button
     const taskRemoveButton = document.createElement("button");
-    taskRemoveButton.innerHTML = "&#128465;";
+    taskRemoveButton.innerHTML = "-";
 
+    // Add all elements to wrapper div and add wrapper div to list
     taskDiv.append(taskCheckbox);
     taskDiv.append(taskLabel);
     taskDiv.append(taskRemoveButton);
     taskList.append(taskDiv);
     numberOfTasks++;
-    /*savedTodos.push({name: taskName, checked: isChecked});
-    localStorage.setItem("todos", JSON.stringify(savedTodos));*/
+    if (isChecked) {
+        taskLabel.classList.add("checked");
+    }
+}
+
+function createNewTask (taskName) {
+    createTask(taskName, false);
+
+    // num of tasks -1 because it's already been incremented
+    savedTodos.push({name: taskName, checked: false, taskId: `task${numberOfTasks-1}`});
+    localStorage.setItem("todos", JSON.stringify(savedTodos));
 }
